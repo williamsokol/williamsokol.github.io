2510c6a7e548bb66d6ee288c50b0f53160901964
PSokol <williamsokol0@gmail.com>
2022-03-25 13:00:46 -0700

    added draft7

---
+++ JsScripts/DriveArticles.js
@@ -0,0 +1,140 @@
+//things to do on start of adding script here:
+
+//include google api
+// var script = document.createElement('script');
+// script.type = 'text/javascript';
+// script.src = 'https://apis.google.com/js/api.js';
+// document.head.appendChild(script);
+// console.log("test");
+
+
+
+get_access_token_using_saved_refresh_token();
+gapi.load('client', initClient);
+
+var GoogleAuth;
+
+function get_access_token_using_saved_refresh_token() {
+    // from the oauth playground
+    const refresh_token = "1//04AaPaGlp2XBgCgYIARAAGAQSNwF-L9IrvDzkHPPpX2-D9FVLPSxY_6p0ulEzldwdqWprNzuoTL6bxHCcoUs5tkJ70WdkzrcDnQA";
+    // from the API console
+    const client_id = "902066689522-hk74eva9n72bg2rgd2eq5mialk3q6o42.apps.googleusercontent.com";
+    // from the API console
+    const client_secret = "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";
+    // from https://developers.google.com/identity/protocols/OAuth2WebServer#offline
+    const refresh_url = "https://www.googleapis.com/oauth2/v4/token";
+
+    const post_body = `grant_type=refresh_token&client_id=${encodeURIComponent(client_id)}&client_secret=${encodeURIComponent(client_secret)}&refresh_token=${encodeURIComponent(refresh_token)}`;
+
+    let refresh_request = {
+        body: post_body,
+        method: "POST",
+        headers: new Headers({
+            'Content-Type': 'application/x-www-form-urlencoded'
+        })
+    }
+
+    // post to the refresh endpoint, parse the json response and use the access token to call files.list
+    fetch(refresh_url, refresh_request).then( response => {
+            return(response.json());
+        }).then( response_json =>  {
+            console.log(response_json);
+            gapi.auth.setToken({
+                access_token: response_json.access_token
+            });
+            //files_list(response_json.access_token);
+    });
+}
+
+function initClient() {
+    GoogleAuth = gapi.client.init({
+      //'apiKey': 'AIzaSyDZJMC3J8GLuXBslOWaewHV3We5cIprCDM',
+      'clientId': '419474436140-1d04mic934dqd0vjakr75be1kukotngh.apps.googleusercontent.com',
+      //'scope': 'https://www.googleapis.com/auth/drive.metadata.readonly',
+      'discoveryDocs': ['https://www.googleapis.com/discovery/v1/apis/drive/v3/rest', 'https://script.googleapis.com/$discovery/rest?version=v1']
+    }).then(function () {                         //I have no idea what this bit on the end is doing, the code works without it.
+        
+        const event  = new CustomEvent('googleLoggedin');
+        document.dispatchEvent(event);
+    
+    });
+    
+    console.log("initiation complete");
+}
+
+function execute() {
+    return gapi.client.drive.files.list({
+        'q' : "'1STx7_sT-bQI9v_ZH3ui6ksc6Lc0VbkQE' in parents",
+        "fields" : "files/webViewLink , files/name, files/id",
+        "orderBy": "createdTime desc"
+    })
+        .then(function(response) {
+        // Handle the results here (response.result has the parsed body).
+        createList(response)
+        //console.log("Response", response);
+        },
+        function(err) { console.error("Execute error", err); });
+}
+function fetchHTMLPage(id){
+    //console.log(id);
+    return gapi.client.drive.files.export({
+        'fileId': id,
+        'mimeType': 'text/html'
+      }).then(function(response){
+          console.log(response.result);
+          turnHTMLToUrl(response.body);
+      });
+}
+
+function turnHTMLToUrl(html) {
+    let doc = document.implementation.createHTMLDocument("New Document");
+    
+    doc.documentElement.innerHTML =  html
+
+    //just fixing little things up
+    changeCss(doc);
+    changeHTML(doc);
+
+    url = URL.createObjectURL(new Blob([doc.documentElement.innerHTML], {type: "text/html"}));
+    // const link = document.createElement('a');
+    // link.href = url;
+    // link.innerText = 'Open the array URL';
+    //document.body.appendChild(link); 
+    const event  = new CustomEvent('loadedArticle');
+    document.dispatchEvent(event);   
+    
+    
+    //window.location.href = url;
+    console.log("loaded article");
+}
+
+function changeCss(doc) {
+    // do it by adding a css file to script
+    //get css file
+    var link = doc.createElement( "link" );
+    link.href = "https://williamsokol.github.io/css/Articles.css";
+    link.rel = "stylesheet";
+    link.type = "text/css";
+    // add css to doc
+    doc.getElementsByTagName( "head" )[0].appendChild( link );
+
+}
+function changeHTML(doc) {
+    // do it by adding a css file to script
+    //get css file
+    var base = doc.createElement( "base" );
+    base.target = "_blank";
+    
+    // add css to doc
+    doc.getElementsByTagName( "head" )[0].appendChild( base );
+
+}
+
+// this will go to a pagme based on it's google doc id
+function GoToPage(id,name = "err no name"){           
+    //sessionStorage.setItem("SelectedArticleId", id);
+    let queryString = "?id="+id+"&name="+ name;
+    window.location.href = "/logpost/MainPostHolder.html"+ queryString;
+}
+//sdads
+
---
+++ js/index.4d650c.js
@@ -0,0 +1,33 @@
+(function(d){var h=[];d.loadImages=function(a,e){"string"==typeof a&&(a=[a]);for(var f=a.length,g=0,b=0;b<f;b++){var c=document.createElement("img");c.onload=function(){g++;g==f&&d.isFunction(e)&&e()};c.src=a[b];h.push(c)}}})(window.jQuery);
+var wl;
+
+ldsrcset=function(t){var e,r=document.querySelectorAll(t);for(e=0;e<r.length;e++){var c=r[e].getAttribute("data-srcset");r[e].setAttribute("srcset",c)}},ldsrc=function(t){var e=document.querySelector(t),r=e.getAttribute("data-src");e.setAttribute("src",r)};ldv=function(t){var e=document.querySelector(t),r=document.querySelector(t+" source"),c=r.getAttribute("data-src");r.setAttribute("src",c),e.play()};!function(){if("Promise"in window&&void 0!==window.performance){var e,t,r=document,n=function(){return r.createElement("link")},o=new Set,a=n(),i=a.relList&&a.relList.supports&&a.relList.supports("prefetch"),s=location.href.replace(/#[^#]+$/,"");o.add(s);var c=function(e){var t=location,r="http:",n="https:";if(e&&e.href&&e.origin==t.origin&&[r,n].includes(e.protocol)&&(e.protocol!=r||t.protocol!=n)){var o=e.pathname;if(!(e.hash&&o+e.search==t.pathname+t.search||"?preload=no"==e.search.substr(-11)||".html"!=o.substr(-5)&&".html"!=o.substr(-5)&&"/"!=o.substr(-1)))return!0}},u=function(e){var t=e.replace(/#[^#]+$/,"");if(!o.has(t)){if(i){var a=n();a.rel="prefetch",a.href=t,r.head.appendChild(a)}else{var s=new XMLHttpRequest;s.open("GET",t,s.withCredentials=!0),s.send()}o.add(t)}},p=function(e){return e.target.closest("a")},f=function(t){var r=t.relatedTarget;r&&p(t)==r.closest("a")||e&&(clearTimeout(e),e=void 0)},d={capture:!0,passive:!0};r.addEventListener("touchstart",function(e){t=performance.now();var r=p(e);c(r)&&u(r.href)},d),r.addEventListener("mouseover",function(r){if(!(performance.now()-t<1200)){var n=p(r);c(n)&&(n.addEventListener("mouseout",f,{passive:!0}),e=setTimeout(function(){u(n.href),e=void 0},80))}},d)}}();
+
+$(function(){
+r=function(){dpi=window.devicePixelRatio;var a='data-src';var e=document.querySelector('.un1');if(e.hasAttribute('src')){a='src';}e.setAttribute(a,(dpi>1)?'images/blue-balloon-218.png':'images/blue-balloon-109.png');
+var a='data-src';var e=document.querySelector('.un2');if(e.hasAttribute('src')){a='src';}e.setAttribute(a,(dpi>1)?'images/asteroid-png-photos-rotated-166.png':'images/asteroid-png-photos-rotated-83.png');
+var a='data-src';var e=document.querySelector('.un3');if(e.hasAttribute('src')){a='src';}e.setAttribute(a,(dpi>1)?'images/pasted-image-198.png':'images/pasted-image-99.png');
+var a='data-src';var e=document.querySelector('.un7');if(e.hasAttribute('src')){a='src';}e.setAttribute(a,(dpi>1)?'images/pasted-image-538.png':'images/pasted-image-269.png');
+var a='data-src';var e=document.querySelector('.un8');if(e.hasAttribute('src')){a='src';}e.setAttribute(a,(dpi>1)?'images/pasted-image-202.png':'images/pasted-image-101.png');
+var a='data-src';var e=document.querySelector('.un9');if(e.hasAttribute('src')){a='src';}e.setAttribute(a,(dpi>1)?'images/pasted-image-362.png':'images/pasted-image-181.png');
+var a='data-src';var e=document.querySelector('.un10');if(e.hasAttribute('src')){a='src';}e.setAttribute(a,(dpi>1)?'images/william-802.png':'images/william-401.png');
+var a='data-src';var e=document.querySelector('.un12');if(e.hasAttribute('src')){a='src';}e.setAttribute(a,(dpi>1)?'images/william-802.png':'images/william-401.png');
+var a='data-src';var e=document.querySelector('.un14');if(e.hasAttribute('src')){a='src';}e.setAttribute(a,(dpi>1)?'images/william-802-1.png':'images/william-401-1.png');
+};
+if(!window.HTMLPictureElement){r();}
+!function(){var e=document.querySelectorAll('a[href^="#"]');[].forEach.call(e,function(e){e.addEventListener("click",function(t){var o=0;if(e.hash.length>1){var l=parseFloat(getComputedStyle(document.body).getPropertyValue("zoom"));l||(l=1);var n=document.querySelectorAll('[name="'+e.hash.slice(1)+'"]')[0];o=(n.getBoundingClientRect().top+pageYOffset)*l}if("scrollBehavior"in document.documentElement.style)scroll({top:o,left:0,behavior:"smooth"});else if("requestAnimationFrame"in window){var r=pageYOffset,a=null;requestAnimationFrame(function e(t){a||(a=t);var l=t-a;scrollTo(0,r<o?(o-r)*l/400+r:r-(r-o)*l/400),l<400?requestAnimationFrame(e):scrollTo(0,o)})}else scrollTo(0,o);t.preventDefault()},!1)})}();
+$('.s63').Stickyfill();
+wl=new woolite();
+wl.init();
+wl.addAnimation($('.un5'), "4.00s", "4.00s", 1, 100);
+wl.addAnimation($('.un6'), "3.00s", "3.00s", 1, 100);
+wl.addAnimation($('.un11'), "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.un13'), "2.00s", "0.00s", 1, 100);
+wl.addAnimation($('.un15'), "3.00s", "0.00s", 1, 100);
+wl.addAnimation($('.un16'), "1.00s", "3.00s", 1, 100);
+wl.addAnimation($('.un18'), "1.00s", "2.00s", 1, 100);
+wl.addAnimation($('.un19'), "1.00s", "1.00s", 1, 100);
+wl.start();
+if(location.hash){var e=location.hash.replace("#",""),o=function(){var t=document.querySelectorAll('[name="'+e+'"]')[0];t&&t.scrollIntoView(),"complete"!=document.readyState&&setTimeout(o,100)};o()}
+
+});lfn=function(){ldsrcset('.un20 source');ldsrcset('.un21 source');ldsrcset('.un22 source');ldsrcset('.un23 source');ldsrcset('.un24 source');ldsrcset('.un25 source');ldsrcset('.un26 source');ldsrcset('.un27 source');ldsrcset('.un28 source');ldsrcset('.un29 source');};if(document.readyState=="complete"){lfn();}else{$(window).on("load",lfn);}
