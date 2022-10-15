590bdf0db521a38e4ec663c27a0472d38df2a521
bobojo <34405532+williamsokol@users.noreply.github.com>
2022-10-15 03:43:15 +0000

    video is able to be change when you click on the left arrow

--- selectProject.js
+++ selectProject.js
@@ -3,9 +3,18 @@ var projFrameList = new Array();
 
 async function test()
 {
-  a = await fetch('https://raw.githubusercontent.com/williamsokol/williamsokol.github.io/websiteV2/Pages/ProjectFrames/RobotMove.html')
+  var a = await fetch('https://raw.githubusercontent.com/williamsokol/williamsokol.github.io/websiteV2/Pages/ProjectFrames/RobotMove.html')
 	//b = await a.json()
-  b = await a.text()
-  console.log(b)
+  var html = await a.text()
+
+  var card = document.getElementById('card');
+  
+  var doc = document.implementation.createHTMLDocument("New Document");  
+  doc.documentElement.innerHTML =  html
+
+  
+  var blobContent = new Blob([ doc.documentElement.innerHTML ], {type: "text/html"})
+  card.src = URL.createObjectURL(blobContent);
+  console.log(card.src)
   
 }
