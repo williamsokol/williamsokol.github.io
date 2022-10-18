9f6519e5ed6c7d14b9122e210862ef411239b259
bobojo <34405532+williamsokol@users.noreply.github.com>
2022-10-18 16:03:02 +0000

    changed link in test.html

--- selectProject.js
+++ selectProject.js
@@ -1,20 +1,21 @@
 var projFrameList = new Array();
 
 
-async function test()
-{
-  var a = await fetch('https://raw.githubusercontent.com/williamsokol/williamsokol.github.io/websiteV2/Pages/ProjectFrames/RobotMove.html')
-	//b = await a.json()
+async function test() {
+  var a = await fetch('https://raw.githubusercontent.com/williamsokol/williamsokol.github.io/websiteV2/Pages/ProjectFrames/test.html')
+  //b = await a.json()
   var html = await a.text()
-
+  console.log(html.length)
   var card = document.getElementById('card');
-  
-  var doc = document.implementation.createHTMLDocument("New Document");  
-  doc.documentElement.innerHTML =  html
-
-  
-  var blobContent = new Blob([ doc.documentElement.innerHTML ], {type: "text/html"})
-  card.src = URL.createObjectURL(blobContent);
+  if(html.length > 500){
+    var doc = document.implementation.createHTMLDocument("New Document");
+    doc.documentElement.innerHTML = html
+    
+    var blobContent = new Blob([doc.documentElement.innerHTML], { type: "text/html" })
+    card.src = URL.createObjectURL(blobContent);
+  }else{
+    card.src = html;
+  }
   console.log(card.src)
-  
+
 }
