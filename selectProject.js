ebeba06389e800af633092aea3ecba240d7d7ebb
bobojo <34405532+williamsokol@users.noreply.github.com>
2022-10-25 05:57:24 +0000

    fix

--- selectProject.js
+++ selectProject.js
@@ -4,30 +4,50 @@ var index = 0;
 //https://api.github.com/repos/williamsokol/williamsokol.github.io/contents/Pages/ProjectFrames?ref=websiteV2
 GetCards();
 async function GetCards() {
-  var a = await fetch('https://api.github.com/repos/williamsokol/williamsokol.github.io/contents/Pages/ProjectFrames?ref=websiteV2')
-  var b = await a.json()
-  projFrameList = b.map(v=>v.download_url);
-  console.log(projFrameList)
+  var pages = await fetch('https://api.github.com/repos/williamsokol/williamsokol.github.io/contents/Pages/ProjectFrames?ref=websiteV2')
+  var b = await pages.json()
+  
+  //projFrameList = b.map(v=>v.url);
+  for(var i=0; i<b.length; i++){
+    var temp = b[i].url
+    var card = []
+    card =  await fetch(temp)
+    card = await card.json();
+    projFrameList.push(card)
+    
+    //console.log(card)
+  }
+  console.log(projFrameList[1].find(v => v.name =="Card.html"))
+  //console.log(projFrameList[1].find(v=> v.name == "Description.html"))
 }
 async function changeCard() { 
   //console.(project)
   index = (index + 1)%(projFrameList.length);
   var a = await fetch(projFrameList[index])
-  //b = await a.json()
-  var html = await a.text()
+  b = await a.json()
+  
+  var html = await
+  
   console.log(html.length)
   var card = document.getElementById('card');
+  var cardDescription = document.getElementById('cardDescription');
   if(html.length > 100)
   {  
-    var doc = document.implementation.createHTMLDocument("New Document");
-    doc.documentElement.innerHTML = html
-    
-    var blobContent = new Blob([doc.documentElement.innerHTML], { type: "text/html" })
-    card.src = URL.createObjectURL(blobContent);
+    card.src = HtmlToBlob(html);
   }else
   {  
     card.src = html;
   }
+  cardDescription.src = 
   console.log(index)
   
+}
+
+function HtmlToBlob(html)
+{
+  var doc = document.implementation.createHTMLDocument("New Document");
+  doc.documentElement.innerHTML = html
+  
+  var blobContent = new Blob([doc.documentElement.innerHTML], { type: "text/html" })
+  return URL.createObjectURL(blobContent);
 }
